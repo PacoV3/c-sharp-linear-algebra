0685e2225b6ac43566f6a2b9972e3ab183e25edb
PacoV3 <franj.vents@gmail.com>
2021-06-03 13:34:44 -0500

    Comit completo del codigo

---
+++ Clase Algebra Lineal/Operaciones.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_Algebra_Lineal
+{
+    class Operaciones
+    {
+        // Atributos
+        public int[,] firstIntDimVector;
+        private int[,] secondIntDimVector;
+        private int escalar;
+
+        // Constructor
+        public Operaciones(int[,] firstIntDimVector, int[,] secondIntDimVector, int escalar)
+        {
+            this.firstIntDimVector = firstIntDimVector;
+            this.secondIntDimVector = secondIntDimVector;
+            this.escalar = escalar;
+        }
+
+        // Metodos
+        // Suma
+        public int[,] sumaMatriz()
+        {
+            int[,] sumVect = new int[firstIntDimVector.GetLength(0), secondIntDimVector.GetLength(0)];
+            for (int i = 0; i < firstIntDimVector.GetLength(0); i++)
+            {
+                for (int j = 0; j < secondIntDimVector.GetLength(1); j++)
+                {
+                    sumVect[i, j] = firstIntDimVector[i, j] + secondIntDimVector[i, j];
+                }
+            }
+            return sumVect;
+        }
+
+        // Resta
+        public int[,] restaMatriz()
+        {
+            int[,] restaVect = new int[firstIntDimVector.GetLength(0), secondIntDimVector.GetLength(0)];
+            for (int i = 0; i < firstIntDimVector.GetLength(0); i++)
+            {
+                for (int j = 0; j < secondIntDimVector.GetLength(1); j++)
+                {
+                    restaVect[i, j] = firstIntDimVector[i, j] - secondIntDimVector[i, j];
+                }
+            }
+            return restaVect;
+        }
+
+        // Escalar
+        public int[,] escalarMatriz()
+        {
+            int[,] escalarVect = new int[firstIntDimVector.GetLength(0), firstIntDimVector.GetLength(1)];
+            for (int i = 0; i < firstIntDimVector.GetLength(0); i++)
+            {
+                for (int j = 0; j < firstIntDimVector.GetLength(1); j++)
+                {
+                    escalarVect[i, j] = escalar * firstIntDimVector[i, j];
+                }
+            }
+            return escalarVect;
+        }
+
+        // Transpuesta
+        public int[,] transpuestaMatriz()
+        {
+            int[,] transpuestaVect = new int[firstIntDimVector.GetLength(1), firstIntDimVector.GetLength(0)];
+            for (int i = 0; i < transpuestaVect.GetLength(0); i++)
+            {
+                for (int j = 0; j < transpuestaVect.GetLength(1); j++)
+                {
+                    transpuestaVect[i, j] = firstIntDimVector[j, i];
+                }
+            }
+            return transpuestaVect;
+        }
+
+        // Determinante con laplace
+        public double laplaceDet(int[,] firstIntDimVector)
+        {
+            double det = 0;
+
+            if (firstIntDimVector.GetLength(1) == 1)
+            {
+                return firstIntDimVector[0, 0];
+            }
+            else
+            {
+                for (int k = 0; k < firstIntDimVector.GetLength(1); k++)
+                {
+                    det = det + Math.Pow(-1, k) * firstIntDimVector[0, k] * laplaceDet(minor(firstIntDimVector, k));
+                }
+            }
+            return det;
+        }
+
+        // Menor complementario para Laplace
+        public int[,] minor(int[,] A, int index2)
+        {
+            int[,] minorC = new int[A.GetLength(0) - 1, A.GetLength(1) - 1];
+
+            for (int i = 1; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    if (index2 == A.GetLength(1) - 1)
+                    {
+                        if (j != A.GetLength(1) - 1)
+                        {
+                            minorC[i - 1, j] = A[i, j];
+                        }
+                    }
+                    else
+                    {
+                        if (j > index2)
+                        {
+                            minorC[i - 1, j - 1] = A[i, j];
+                        }
+                        else
+                        {
+                            if (index2 == 0)
+                            {
+                                minorC[i - 1, j] = A[i - 1, j + 1];
+                            }
+                            else
+                            {
+                                minorC[i - 1, j] = A[i, j];
+                            }
+                        }
+                    }
+                }
+            }
+            return minorC;
+        }
+        
+        // Multiplicacion de matriz
+        public int[,] multMatriz()
+        {
+            int[,] c = new int[firstIntDimVector.GetLength(0), secondIntDimVector.GetLength(1)];
+            int suma = 0;
+            for (int i = 0; i < firstIntDimVector.GetLength(0); i++)
+            {
+                for (int j = 0; j < secondIntDimVector.GetLength(1); j++)
+                {
+                    for (int k = 0; k < secondIntDimVector.GetLength(0); k++)
+                    {
+                        suma = suma + (firstIntDimVector[i, k] * secondIntDimVector[k, j]);
+                    }
+                    c[i, j] = suma;
+                    suma = 0;
+                }
+            }
+            return c;
+        }
+    }
+}
---
+++ Clase Algebra Lineal/Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_Algebra_Lineal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaras valores para trabajar y objetos para usar
+            Operaciones Op1, Op2;
+            int escalar;
+
+            // Leer las matrices
+            string inputText;
+            string[] rengColumVal;
+            Console.WriteLine("\t\t\t\t\t\t«Calculadora de Matrices»");
+            Console.Write("Renglones y columnas de la matriz A: ");
+            inputText = Console.ReadLine();
+            rengColumVal = inputText.Split(' ');
+
+            int[,] a = new int[int.Parse(rengColumVal[0]), int.Parse(rengColumVal[1])];
+            pedirMatriz(a);
+
+            Console.Write("\nRenglones y columnas de la matriz B: ");
+            inputText = Console.ReadLine();
+            rengColumVal = inputText.Split(' ');
+
+            int[,] b = new int[int.Parse(rengColumVal[0]), int.Parse(rengColumVal[1])];
+            pedirMatriz(b);
+
+            Console.Write("\nValor del escalar: ");
+            escalar = int.Parse(Console.ReadLine());
+
+            Op1 = new Operaciones(a, b, escalar);
+            Op2 = new Operaciones(b, a, escalar);
+
+            // Validaciones para usar los metodos correctos
+            if(a.GetLength(0) != b.GetLength(0) || a.GetLength(1) != b.GetLength(1))
+            {
+                int[,] escalarA = Op1.escalarMatriz();
+                int[,] escalarB = Op2.escalarMatriz();
+
+                int[,] transpuestaA = Op1.transpuestaMatriz();
+                int[,] transpuestaB = Op2.transpuestaMatriz();
+
+                // Imprimir los datos de cada matriz
+                Console.WriteLine("\nMatriz A");
+                imprimirMatriz(a);
+                Console.WriteLine("\nMatriz B");
+                imprimirMatriz(b);
+
+                if (a.GetLength(1) == b.GetLength(0))
+                {
+                    int[,] multiplicacion = Op1.multMatriz();
+
+                    Console.WriteLine("\nA X B");
+                    imprimirMatriz(multiplicacion);
+                }
+                else
+                    Console.WriteLine("\nNo se puede calcular A X B, comprueba las columnas y filas de tus matrices");
+
+
+                Console.WriteLine("\nEscalar de A con " + escalar);
+                imprimirMatriz(escalarA);
+                Console.WriteLine("\nEscalar de B con " + escalar);
+                imprimirMatriz(escalarB);
+
+                Console.WriteLine("\nTranspuesta de la matriz A");
+                imprimirMatriz(transpuestaA);
+                Console.WriteLine("\nTranspuesta de la matriz B");
+                imprimirMatriz(transpuestaB);
+
+                // Calcular Determinante
+                if(a.GetLength(0) == a.GetLength(1) && b.GetLength(0) == b.GetLength(1))
+                {
+                    double detA = Op1.laplaceDet(a);
+                    double detB = Op2.laplaceDet(a);
+
+                    Console.WriteLine("\nDeterminante de la matriz A");
+                    Console.WriteLine(detA);
+                    Console.WriteLine("\nDeterminante de la matriz B");
+                    Console.WriteLine(detB);
+                }
+                else
+                {
+                    if(a.GetLength(0) == a.GetLength(1))
+                    {
+                        double detA = Op1.laplaceDet(a);
+                        Console.WriteLine("\nDeterminante de la matriz A");
+                        Console.WriteLine(detA);
+                        Console.WriteLine("\nB tiene que ser una matriz cuadrada para calcular su determinante");
+                    }
+                    else
+                    {
+                        if(b.GetLength(0) == b.GetLength(1))
+                        {
+                            double detB = Op2.laplaceDet(b);
+                            Console.WriteLine("\nDeterminante de la matriz B");
+                            Console.WriteLine(detB);
+                            Console.WriteLine("\nA tiene que ser una matriz cuadrada para calcular su determinante");
+                        }
+                        else
+                            Console.WriteLine("\nA y B tienen que ser matrices cuadradas para calcular su determinante");
+                    }
+                }
+            }
+            else
+            {
+                int[,] suma = Op1.sumaMatriz();
+                int[,] restaAB = Op1.restaMatriz();
+                int[,] restaBA = Op2.restaMatriz();
+
+                int[,] escalarA = Op1.escalarMatriz();
+                int[,] escalarB = Op2.escalarMatriz();
+
+                int[,] transpuestaA = Op1.transpuestaMatriz();
+                int[,] transpuestaB = Op2.transpuestaMatriz();
+
+                Console.WriteLine("\nMatriz A");
+                imprimirMatriz(a);
+                Console.WriteLine("\nMatriz B");
+                imprimirMatriz(b);
+
+                Console.WriteLine("\nA + B");
+                imprimirMatriz(suma);
+                Console.WriteLine("\nA - B");
+                imprimirMatriz(restaAB);
+                Console.WriteLine("\nB - A");
+                imprimirMatriz(restaAB);
+
+                if (a.GetLength(1) == b.GetLength(0))
+                {
+                    int[,] multiplicacion = Op1.multMatriz();
+
+                    Console.WriteLine("\nA X B");
+                    imprimirMatriz(multiplicacion);
+                }
+                else
+                    Console.WriteLine("\nNo se puede calcular A X B, comprueba las columnas y filas de tus matrices");
+
+                Console.WriteLine("\nEscalar de A con " + escalar);
+                imprimirMatriz(escalarA);
+                Console.WriteLine("\nEscalar de B con " + escalar);
+                imprimirMatriz(escalarB);
+
+                Console.WriteLine("\nTranspuesta de la matriz A");
+                imprimirMatriz(transpuestaA);
+                Console.WriteLine("\nTranspuesta de la matriz B");
+                imprimirMatriz(transpuestaB);
+
+                // Calcular Determinante
+                if (a.GetLength(0) == a.GetLength(1) && b.GetLength(0) == b.GetLength(1))
+                {
+                    double detA = Op1.laplaceDet(a);
+                    double detB = Op2.laplaceDet(a);
+
+                    Console.WriteLine("\nDeterminante de la matriz A");
+                    Console.WriteLine(detA);
+                    Console.WriteLine("\nDeterminante de la matriz B");
+                    Console.WriteLine(detB);
+                }
+                else
+                {
+                    if (a.GetLength(0) == a.GetLength(1))
+                    {
+                        double detA = Op1.laplaceDet(a);
+                        Console.WriteLine("\nDeterminante de la matriz A");
+                        Console.WriteLine(detA);
+                        Console.WriteLine("\nB tiene que ser una matriz cuadrada para calcular su determinante");
+                    }
+                    else
+                    {
+                        if (b.GetLength(0) == b.GetLength(1))
+                        {
+                            double detB = Op2.laplaceDet(b);
+                            Console.WriteLine("\nDeterminante de la matriz B");
+                            Console.WriteLine(detB);
+                            Console.WriteLine("\nA tiene que ser una matriz cuadrada para calcular su determinante");
+                        }
+                        else
+                            Console.WriteLine("\nA y B tienen que ser matrices cuadradas para calcular su determinante");
+                    }
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+        
+        // Funcion void para imprimir el contenido de una matriz
+        public static void imprimirMatriz(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Funcion para leer los valores linea por linea y almacenarlos
+        public static void pedirMatriz(int[,] a)
+        {
+            string inputText;
+            string[] rengColumVal;
+
+            Console.WriteLine("Valores de la matriz: ");
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                inputText = Console.ReadLine();
+                rengColumVal = inputText.Split(' ');
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    a[i, j] = int.Parse(rengColumVal[j]);
+                }
+            }
+        }
+    }
+    // Francisco Javier Ventura Segura 
+}
